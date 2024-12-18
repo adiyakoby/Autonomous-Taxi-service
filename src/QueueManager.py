fd8a9c3db20c91ece66aedd1c6e64bc3150d6656
Adi Yakoby <adi.yakoby@gmail.com>
2024-12-18 15:18:22 +0200

    init queue manager and riderequests

---
+++ src/QueueManager.py
@@ -0,0 +1,18 @@
+from collections import deque
+from RideRequest import RideRequest
+
+MIN_VAL = 0
+MAX_VAL = 20000
+
+class QueueManager:
+    
+    def __init__(self):
+        self._que = deque()
+    
+    
+    def add_request(self, req: RideRequest) -> None:
+        pos, dest = req.get_pos(), req.get_dest()
+        if not (MIN_VAL <= pos[0] <= MAX_VAL and MIN_VAL <= pos[1] <= MAX_VAL) or \
+           not (MIN_VAL <= dest[0] <= MAX_VAL and MIN_VAL <= dest[1] <= MAX_VAL):
+               raise ValueError(f'Values of position and destination must be between {MIN_VAL} to {MAX_VAL}.')
+               
---
+++ src/RideRequest.py
@@ -0,0 +1,15 @@
+
+
+class RideRequest:
+
+    def __init__(self, start: tuple[int, int], end: tuple[int, int]):
+        if type(start) != tuple or type(end) != tuple or len(start) != 2 or len(end) != 2:
+            raise TypeError('Must provide two tuples of length 2')
+        self._pos = start
+        self._dest = end
+        
+    def get_pos(self) -> tuple[int, int]:
+        return self._pos
+    
+    def get_dest(self) -> tuple[int, int]:
+        return self._dest
